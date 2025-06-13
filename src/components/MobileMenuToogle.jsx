131e0dfcc3b0f89808763e5adb52ef4891604a5e
seticap <59266839+seticap@users.noreply.github.com>
2025-06-13 13:06:39 -0500

    Create MobileMenuToogle.jsx
    para el menu hamburguesa

---
+++ src/components/MobileMenuToogle.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+const MobileMenuToggle = ({ open, toggle }) => {
+  return (
+    <button
+      className="sm:hidden text-3xl absolute right-4 top-4 z-50"
+      onClick={toggle}
+      aria-label={open ? "Cerrar menú" : "Abrir menú"}
+    >
+      {open ? "✕" : "☰"}
+    </button>
+  );
+};
+
+export default MobileMenuToggle;
