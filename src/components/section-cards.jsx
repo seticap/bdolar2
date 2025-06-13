ee7671aceaa54bff81223d8746a88524abc8aef9
seticap <59266839+seticap@users.noreply.github.com>
2025-06-13 12:57:26 -0500

    Update section-cards.jsx
    Responsividad para el componente section-cards

--- src/components/section-cards.jsx
+++ src/components/section-cards.jsx
@@ -11,6 +11,15 @@ export function SectionCards() {
   const [preciosAyer, setPreciosAyer] = useState({});
 
   useEffect(() => {
+    const loadData = () => {
+      try {
+        const data = localStorage.getItem("ayer-1006");
+        if (data) setPreciosAyer(JSON.parse(data));
+      } catch (e) {
+        console.error("Error loading localStorage data:", e);
+      }
+    };
+
     if (typeof window !== "undefined") {
       const data = localStorage.getItem("ayer-1006");
       if (data) {
@@ -26,7 +35,7 @@ export function SectionCards() {
   //LOG INFORMACION DE PRECIOS
   // console.log(preciosHoy);
   // console.log(preciosAyer);
-  
+
   const montos = dataById[1005]?.data || {};
   const promedio = dataById[1007]?.data || {};
 
@@ -36,7 +45,6 @@ export function SectionCards() {
     }
     return parseFloat(valor);
   };
-
 
   const getVar = (hoy, ayer) => {
     const numHoy = limpiarNumero(hoy);
@@ -48,179 +56,209 @@ export function SectionCards() {
     return varp.toFixed(2);
   };
 
-  const horasOrdenas = Object.keys(historico || {})
-    .filter((h) => {
-      const hNum = parseInt(h.split(":")[0]);
-      return hNum >= 9 && hNum <= 12;
-    })
-    .sort();
+  // const horasOrdenas = Object.keys(historico || {})
+  //   .filter((h) => {
+  //     const hNum = parseInt(h.split(":")[0]);
+  //     return hNum >= 9 && hNum <= 12;
+  //   })
+  //   .sort();
 
   return (
-    <div className="text-white grid grid-cols-1 gap-4 px-4 lg:px-1 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-      <Card className="w-[320px] bg-custom-colortwo text-white border-none p-4">
-        <CardContent className="p-0 space-y-4">
-          {/* Tabla de Precios */}
-          <div className="font-medium mb-2 text-[18px]">PRECIOS</div>
-          <div className="text-sm">
-            <table className="w-full">
-              <thead>
-                <tr className="border-b border-gray-600">
-                  <th className="text-left pb-1"></th>
-                  <th className="text-right pb-1">HOY</th>
-                  <th className="text-right pb-1">AYER</th>
-                  <th className="text-right pb-1">VAR%</th>
-                </tr>
-              </thead>
-              <tbody>
-                {[
-                  {
-                    label: "CIERRE",
-                    hoy: "4,168.03",
-                    ayer: "965,50",
-                  },
-                  {
-                    label: "MÁXIMO",
-                    hoy: preciosHoy.high,
-                    ayer: preciosAyer.high,
-                  },
-                  {
-                    label: "MÍNIMO",
-                    hoy: preciosHoy.low,
-                    ayer: preciosAyer.low,
-                  },
-                  {
-                    label: "APERTURA",
-                    hoy: preciosHoy.open,
-                    ayer: preciosAyer.open,
-                  },
-                ].map(({ label, hoy, ayer }) => (
-                  <tr key={label} className="border-b border-gray-600">
-                    <td className="py-1.5">{label}</td>
-                    <td className="text-right">{hoy}</td>
-                    <td className="text-right">{ayer}</td>
-                    <td className="text-right"
-                      style={{ color: getVar(hoy, ayer) < 0 ? "red" : "green" }}
-                    >
-                      {getVar(hoy, ayer)}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+    <div
+      className="text-white w-full max-w-screen-2xl mx-auto 
+                 px-4 sm:px-30 md:px-25 lg:px-0"
+    >
+      <div className="grid grid-cols-1 @sm:grid-cols-2 @xl/main:grid-cols-4 gap-4 w-full">
+        <Card className="w-full h-auto @sm:h-[580px] bg-custom-colortwo text-white border-none p-3 @sm:p-4">
+          <CardContent className="p-0 space-y-3 @sm:space-y-4">
+            {/* Tabla de Precios */}
+            <div className="font-medium text-base sm:text-lg">PRECIOS</div>
 
-          {/* Montos USD y Transacciones */}
-          <div className=" mb-2 ">
-            <div className="text-sm">
-              <div className="font-medium mb-1">MONTOS USD:</div>
-              <table className="w-full">
+            <div className="text-sm overflow-x-auto">
+              <table className="w-full min-w-[280px]">
                 <thead>
-                  <tr>
-                    <th></th>
-
-                    <th className="text-right px-5">HOY</th>
+                  <tr className="border-b border-gray-600">
+                    <th className="text-left pb-1"></th>
+                    <th className="text-right pb-1">HOY</th>
+                    <th className="text-right pb-1">AYER</th>
+                    <th className="text-right pb-1">VAR%</th>
                   </tr>
                 </thead>
                 <tbody>
                   {[
-                    { label: "NEGOCIADO", hoy: montos.sum },
-                    { label: "ULTIMO", hoy: montos.close },
-                    { label: "PROMEDIO", hoy: montos.avg },
-                    { label: "MAXIMO", hoy: montos.high },
-                    { label: "MINIMO", hoy: montos.low },
-                    { label: "TRANSACCIONES", hoy: montos.count },
-                  ].map(({ label, hoy }) => (
+                    {
+                      label: "CIERRE",
+                      hoy: "4,168.03",
+                      ayer: "965,50",
+                    },
+                    {
+                      label: "MÁXIMO",
+                      hoy: preciosHoy.high,
+                      ayer: preciosAyer.high,
+                    },
+                    {
+                      label: "MÍNIMO",
+                      hoy: preciosHoy.low,
+                      ayer: preciosAyer.low,
+                    },
+                    {
+                      label: "APERTURA",
+                      hoy: preciosHoy.open,
+                      ayer: preciosAyer.open,
+                    },
+                  ].map(({ label, hoy, ayer }) => (
                     <tr key={label} className="border-b border-gray-600">
-                      <td className="py-1">{label}</td>
+                      <td className="py-1.5">{label}</td>
                       <td className="text-right">{hoy}</td>
+                      <td className="text-right">{ayer}</td>
+                      <td
+                        className="text-right"
+                        style={{
+                          color: getVar(hoy, ayer) < 0 ? "red" : "green",
+                        }}
+                      >
+                        {getVar(hoy, ayer)}
+                      </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
-          </div>
 
+            {/* Montos USD y Transacciones */}
+            <div className=" mb-2 ">
+              <div className="text-sm">
+                <div className="font-medium mb-1">MONTOS USD:</div>
+                <table className="w-full">
+                  <thead>
+                    <tr>
+                      <th></th>
 
-          {/* Tabla Actual */}
-          <div className="text-sm">
-            <table className="w-full">
-              <thead>
-                <tr className="border-b border-gray-600">
-                  <th className="text-left pb-1"></th>
-                  <th className="text-left pb-1">HORA</th>
-                  <th className="text-left pb-1">PROMEDIO</th>
-                  <th className="text-left pb-1">CIERRE</th>
-                </tr>
-              </thead>
-              <tbody>
-                {horasOrdenas.map((hora) => (
-                  <tr key={hora} className="text-center border-b border-gray-700">
-                    <td className="py-1.5">{hora}</td>
-                    <td className="text-right ">${historico[hora].avg}</td>
-                    <td className="text-right ">${historico[hora].close}</td>
-                  </tr>
-                ))}
-                
-                
-              </tbody>
-            </table>
-          </div>
-        </CardContent>
-      </Card>
-
-
-      {/* MONEDAS */}
-      <Card className="w-[320px] h-[200px] text-white bg-custom-colortwo border-none p-4">
-        <CardHeader className="p-0 mb-3">
-          <CardTitle className="text-lg font-semibold">MONEDAS</CardTitle>
-        </CardHeader>
-        <CardContent className="p-0 overflow-auto h-[calc(100%-40px)] scrollbar-custom">
-          <div className="space-y-2">
-            <div className="flex justify-between text-gray-400 text-xs p-1 border-b border-gray-600">
-              <span className="w-[55%]">DIVISAS</span>
-              <span className="w-[25%] text-right">VALOR</span>
-              <span className="w-[20%] text-right">%</span>
+                      <th className="text-right px-5">HOY</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {[
+                      { label: "NEGOCIADO", hoy: montos.sum },
+                      { label: "ULTIMO", hoy: montos.close },
+                      { label: "PROMEDIO", hoy: montos.avg },
+                      { label: "MAXIMO", hoy: montos.high },
+                      { label: "MINIMO", hoy: montos.low },
+                      { label: "TRANSACCIONES", hoy: montos.count },
+                    ].map(({ label, hoy }) => (
+                      <tr key={label} className="border-b border-gray-600">
+                        <td className="py-1">{label}</td>
+                        <td className="text-right">{hoy}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
 
-            {[
-              { moneda: "DÓLAR SPOT", valor: "0,9537", cambio: "-0.1160" },
-              { moneda: "DÓLAR OBS", valor: "0,9537", cambio: "-0.1160" },
-              { moneda: "EURO", valor: "5,7042", cambio: "+0.0009" },
-              { moneda: "PESO COL", valor: "20,2880", cambio: "-0.1160" },
-              { moneda: "REAL BRL", valor: "152,1680", cambio: "+0.0013" },
-              { moneda: "PESO ARG", valor: "0,9537", cambio: "+0.0009" },
-              { moneda: "SOL PERUANO", valor: "5,7042", cambio: "-0.0059" },
-              { moneda: "PESO MXN", valor: "20,2880", cambio: "+0.0013" },
-              { moneda: "DÓLAR AUS", valor: "152,1680", cambio: "-0.0059" },
-              { moneda: "DÓLAR CAD", valor: "5,7042", cambio: "-0.0059" },
-              {
-                moneda: "LIBRA ESTERLINA",
-                valor: "20,2880",
-                cambio: "-0.0059",
-              },
-            ].map((item, index) => (
-              <div
-                key={index}
-                className="flex justify-between items-center border-b border-gray-700 hover:bg-gray-700/50 transition-colors"
-              >
-                <span className="w-[55%]">{item.moneda}</span>
-                <span className="w-[25%] text-right font-mono">
-                  {item.valor}
-                </span>
-                <span
-                  className={`w-[20%] text-right font-mono ${
-                    item.cambio.startsWith("+")
-                      ? "text-green-400"
-                      : "text-red-400"
-                  }`}
-                >
-                  {item.cambio}
-                </span>
-              </div>
-            ))}
+            {/* Tabla Actual */}
+            <div className="overflow-x-auto text-sm">
+              <table className="min-w-[280px] w-full">
+                <thead>
+                  <tr className="border-b border-gray-600">
+                    <th className="text-center px-3 pb-1">HORA</th>
+                    <th className="text-center pb-1">PROMEDIO</th>
+                    <th className="text-center px-5 pb-1">CIERRE</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {["09:00", "10:00", "11:00", "12:00"].map((hora) => (
+                    <tr key={hora} className="border-b border-gray-700">
+                      <td className="py-1.5 text-center">{hora}</td>
+                      <td className="text-right pr-7">
+                        {historico[hora] ? `${historico[hora].avg}` : "-"}
+                      </td>
+                      <td className="text-right pr-4">
+                        {historico[hora] ? `${historico[hora].close}` : "-"}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </CardContent>
+        </Card>
+
+        {/* MONEDAS (PROVISIONAL ANTES DE CAMBIARLO A LA API) */}
+        <Card
+          className="w-full max-w-full h-auto sm:h-[200px] mx-auto text-white bg-custom-colortwo border-none 
+                p-3 sm:p-4 md:p-5 lg:p-2"
+        >
+          <CardHeader className="p-0 mb-[-18]">
+            <CardTitle className="text-lg font-semibold">MONEDAS</CardTitle>
+          </CardHeader>
+          <div className="flex justify-between text-gray-400 text-xs p-1 border-b border-gray-600">
+            <span className="w-[55%]">DIVISAS</span>
+            <span className="w-[25%] text-right">VALOR</span>
+            <span className="w-[20%] text-right px-5">%</span>
           </div>
-        </CardContent>
-      </Card>
+          <CardContent className="p-0 overflow-auto max-h-[300px] sm:max-h-[calc(100%-40px)] scrollbar-custom">
+            <div className="py-1 space-y-1 text-sm">
+              {[
+                { moneda: "DÓLAR SPOT", valor: "0,9537", cambio: "-0.1160" },
+                { moneda: "DÓLAR OBS", valor: "0,9537", cambio: "-0.1160" },
+                { moneda: "EURO", valor: "5,7042", cambio: "+0.0009" },
+                { moneda: "PESO COL", valor: "20,2880", cambio: "-0.1160" },
+                { moneda: "REAL BRL", valor: "152,1680", cambio: "+0.0013" },
+                { moneda: "PESO ARG", valor: "0,9537", cambio: "+0.0009" },
+                { moneda: "SOL PERUANO", valor: "5,7042", cambio: "-0.0059" },
+                { moneda: "PESO MXN", valor: "20,2880", cambio: "+0.0013" },
+                { moneda: "DÓLAR AUS", valor: "152,1680", cambio: "-0.0059" },
+                { moneda: "DÓLAR CAD", valor: "5,7042", cambio: "-0.0059" },
+                { moneda: "PESO COL", valor: "20,2880", cambio: "-0.1160" },
+                { moneda: "REAL BRL", valor: "152,1680", cambio: "+0.0013" },
+                { moneda: "PESO ARG", valor: "0,9537", cambio: "+0.0009" },
+                { moneda: "SOL PERUANO", valor: "5,7042", cambio: "-0.0059" },
+                { moneda: "PESO MXN", valor: "20,2880", cambio: "+0.0013" },
+                {
+                  moneda: "LIBRA ESTERLINA",
+                  valor: "20,2880",
+                  cambio: "-0.0059",
+                },
+                { moneda: "DÓLAR SPOT", valor: "0,9537", cambio: "-0.1160" },
+                { moneda: "DÓLAR OBS", valor: "0,9537", cambio: "-0.1160" },
+                { moneda: "EURO", valor: "5,7042", cambio: "+0.0009" },
+                { moneda: "PESO COL", valor: "20,2880", cambio: "-0.1160" },
+                { moneda: "REAL BRL", valor: "152,1680", cambio: "+0.0013" },
+                { moneda: "PESO ARG", valor: "0,9537", cambio: "+0.0009" },
+                { moneda: "SOL PERUANO", valor: "5,7042", cambio: "-0.0059" },
+                { moneda: "PESO MXN", valor: "20,2880", cambio: "+0.0013" },
+                { moneda: "DÓLAR AUS", valor: "152,1680", cambio: "-0.0059" },
+                { moneda: "DÓLAR CAD", valor: "5,7042", cambio: "-0.0059" },
+                { moneda: "PESO COL", valor: "20,2880", cambio: "-0.1160" },
+                { moneda: "REAL BRL", valor: "152,1680", cambio: "+0.0013" },
+                { moneda: "PESO ARG", valor: "0,9537", cambio: "+0.0009" },
+                { moneda: "SOL PERUANO", valor: "5,7042", cambio: "-0.0059" },
+                { moneda: "PESO MXN", valor: "20,2880", cambio: "+0.0013" },
+              ].map((item, index) => (
+                <div
+                  key={index}
+                  className="flex justify-between items-center border-b border-gray-700 hover:bg-gray-700/50 transition-colors"
+                >
+                  <span className="w-[55%]">{item.moneda}</span>
+                  <span className="w-[25%] text-right font-mono">
+                    {item.valor}
+                  </span>
+                  <span
+                    className={`w-[20%] text-right font-mono ${
+                      item.cambio.startsWith("+")
+                        ? "text-green-400"
+                        : "text-red-400"
+                    }`}
+                  >
+                    {item.cambio}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
