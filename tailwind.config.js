132ad53473b567e4c0440e924be7888949542dc9
seticap <59266839+seticap@users.noreply.github.com>
2025-06-13 13:10:58 -0500

    new file tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  theme: {
+    extend: {
+      margin: {
+        "1p": "1%",
+        "2p": "2%",
+        "3p": "3%",
+        "4p": "4%",
+        "5p": "5%",
+        "6p": "6%",
+        "7p": "7%",
+        "8p": "8%",
+        "9p": "9%",
+        "10p": "10%",
+        "15p": "15%",
+        "20p": "20%",
+      },
+      padding: {
+        "5p": "5%",
+        "10p": "10%",
+        "15p": "15%",
+      },
+    },
+  },
+  plugins: [],
+};
