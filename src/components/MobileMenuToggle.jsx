8a73c4b4e26544724f466e56db8df67fd80caeea
seticap <59266839+seticap@users.noreply.github.com>
2025-07-11 11:20:59 -0500

    Update MobileMenuToggle.jsx
    Ajuste visual del icono del menú hamburguesa

--- src/components/MobileMenuToggle.jsx
+++ src/components/MobileMenuToggle.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const MobileMenuToggle = ({ open, toggle }) => {
   return (
     <button
-      className="sm:hidden text-3xl absolute right-4 top-4 z-50"
+      className="sm:hidden text-5xl absolute right-5 top-6 z-50"
       onClick={toggle}
       aria-label={open ? "Cerrar menú" : "Abrir menú"}
     >
